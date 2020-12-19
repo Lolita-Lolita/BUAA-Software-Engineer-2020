aca89f49dd6441c00d0766863a7421c4ee07827f
BerlinDorin <876656787@qq.com>
2020-12-19 18:09:23 +0800

    初步完成日志模块接入

--- backend/dish/src/main/java/com/example/dish/controller/DishController.java
+++ backend/dish/src/main/java/com/example/dish/controller/DishController.java
@@ -5,7 +5,8 @@ import com.example.dish.service.DishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import javax.persistence.GeneratedValue;
 import javax.persistence.PostLoad;
 import java.util.List;
@@ -16,7 +17,22 @@ import java.util.Optional;
 public class DishController {
     @Autowired
     private DishService dishService;
+    private static Logger logger= LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
 
+    @GetMapping("/getLog")
+    public void testLog(){
+        for(int i=0;i<3;i++){
+            // 记录trace级别的信息
+            logger.trace("log4j2日志输出：This is trace message.");
+            // 记录debug级别的信息
+            logger.debug("log4j2日志输出：This is debug message.");
+            // 记录info级别的信息
+            logger.info("log4j2日志输出：This is info message.");
+            // 记录error级别的信息
+            logger.error("log4j2日志输出：This is error message.");
+        }
+
+    }
     @GetMapping("/id/{id}")
     public ResponseEntity<Optional<Dish>> findByID(@PathVariable("id") Integer id) {
         return ResponseEntity.ok(dishService.findById(id));
