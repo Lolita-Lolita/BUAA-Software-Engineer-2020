3f4146055472eb2c24117ba530771ea499d77b10
李想 <214481008@qq.com>
2020-12-26 11:01:54 +0800

    3 ms registered on eureka server

--- backend/dish/src/main/java/com/example/dish/DishApplication.java
+++ backend/dish/src/main/java/com/example/dish/DishApplication.java
@@ -3,13 +3,20 @@ package com.example.dish;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class DishApplication
 {
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(DishApplication.class, args);
