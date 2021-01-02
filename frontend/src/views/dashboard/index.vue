053406e852b573b2d1fbde7e0795ce6c2cbe3e08
chhh <815912171@qq.com>
2021-01-03 00:10:49 +0800

    修改了主页

--- frontend/src/views/dashboard/index.vue
+++ frontend/src/views/dashboard/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <div class="dashboard-container">
-      <div class="dashboard-text">name: {{ name }}</div>
+      <div class="dashboard-text">欢迎您，{{ name }}！今天您恰了吗？</div>
+      <el-divider></el-divider>
+      <div class="dashboard-quote">
+        <div class="dashboard-quote-text">君来坐树下，饱食携其余。</div>
+        <div class="dashboard-quote-text">归舍遗儿子，怀抱不可虚。</div>
+        <div class="dashboard-quote-text">——苏轼《和陶归园田居》</div>
+      </div>      
     </div>
     <div class="background" :style="backgroundStyle">
     </div>
@@ -38,6 +44,18 @@ export default {
   &-text {
     font-size: 30px;
     line-height: 46px;
+  }
+  &-quote {
+    padding: 8px 16px;
+    background-color: #ecf8ff;
+    border-radius: 4px;
+    border-left: 5px solid #50bfff;
+    margin: 20px 0;
+    &-text{
+      margin: 20px 0;
+      font-size: 2em;
+      font-family: '华文仿宋','Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
   }
 }
 .background{
