13e5c3006c69b69354e9f4b17629bc4c479eabdb
Jiaqian <janelin9712@163.com>
2021-01-08 13:28:31 +0800

    再次修改主页

--- frontend/src/views/dashboard/index.vue
+++ frontend/src/views/dashboard/index.vue
@@ -7,8 +7,8 @@
         <el-row :gutter="40" class="panel-group">
           <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
             <div class="card-panel">
-              <div class="card-panel-icon-wrapper icon-dashboard">
-                <svg-icon icon-class="dashboard" class-name="card-panel-icon" />
+              <div class="card-panel-icon-wrapper icon-dog">
+                <svg-icon icon-class="food" class-name="card-panel-icon" />
               </div>
               <div class="card-panel-description">
                 <div class="card-panel-text">
@@ -20,8 +20,8 @@
           </el-col>
           <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
             <div class="card-panel">
-              <div class="card-panel-icon-wrapper icon-people">
-                <svg-icon icon-class="peoples" class-name="card-panel-icon" />
+              <div class="card-panel-icon-wrapper icon-dog">
+                <svg-icon icon-class="doglike" class-name="card-panel-icon" />
               </div>
               <div class="card-panel-description">
                 <div class="card-panel-text">
@@ -33,8 +33,8 @@
           </el-col>
           <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
             <div class="card-panel">
-              <div class="card-panel-icon-wrapper icon-star">
-                <svg-icon icon-class="star" class-name="card-panel-icon" />
+              <div class="card-panel-icon-wrapper icon-dog">
+                <svg-icon icon-class="dogrun" class-name="card-panel-icon" />
               </div>
               <div class="card-panel-description">
                 <div class="card-panel-text">
@@ -46,8 +46,8 @@
           </el-col>
           <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
             <div class="card-panel">
-              <div class="card-panel-icon-wrapper icon-shopping">
-                <svg-icon icon-class="bug" class-name="card-panel-icon" />
+              <div class="card-panel-icon-wrapper icon-dog">
+                <svg-icon icon-class="dog" class-name="card-panel-icon" />
               </div>
               <div class="card-panel-description">
                 <div class="card-panel-text">
@@ -59,11 +59,39 @@
           </el-col>
         </el-row>
       </div>
-      <div class="dashboard-quote">
-        <div class="dashboard-quote-text">君来坐树下，饱食携其余。</div>
-        <div class="dashboard-quote-text">归舍遗儿子，怀抱不可虚。</div>
-        <div class="dashboard-quote-text">——苏轼《和陶归园田居》</div>
-      </div>
+      <el-row :gutter="500">
+        <el-col :span="6">
+          <div class="dashboard-quote">
+            <div class="dashboard-quote-text">君来坐树下，饱食携其余。</div>
+            <div class="dashboard-quote-text">归舍遗儿子，怀抱不可虚。</div>
+            <div class="dashboard-quote-text">——苏轼《和陶归园田居》</div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="dashboard-quote">
+            <div class="dashboard-quote-text">江上往来人，但爱鲈鱼美。</div>
+            <div class="dashboard-quote-text">君看一叶舟，出入风波里。</div>
+            <div class="dashboard-quote-text">——范仲淹《江上渔者》</div>
+          </div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="500">
+        <el-col :span="6">
+          <div class="dashboard-quote">
+            <div class="dashboard-quote-text">春种一粒粟，秋收万颗子。</div>
+            <div class="dashboard-quote-text">四海无闲田，农夫犹饿死。</div>
+            <div class="dashboard-quote-text">——李绅《悯农·其一》</div>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="dashboard-quote">
+            <div class="dashboard-quote-text">锄禾日当午， 汗滴禾下土。</div>
+            <div class="dashboard-quote-text">谁知盘中餐， 粒粒皆辛苦。</div>
+            <div class="dashboard-quote-text">——李绅《悯农·其二》</div>
+          </div>
+        </el-col>
+      </el-row>
+
     </div>
     <div class="background" :style="backgroundStyle" />
   </div>
@@ -109,13 +137,15 @@ export default {
   }
   &-quote {
     padding: 8px 16px;
-    background-color: #ecf8ff;
+    background-color: 	rgb(176,196,222,0.15);
     border-radius: 4px;
-    border-left: 5px solid #50bfff;
+    border-left: 5px solid #FFDEAD;
     margin: 20px 0;
+    width: 400px;
+    box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
     &-text{
       margin: 20px 0;
-      font-size: 2em;
+      font-size: 1.5em;
       font-family: '华文仿宋','Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     }
   }
@@ -151,37 +181,9 @@ export default {
         color: #fff;
       }
 
-      .icon-people {
-        background: #40c9c6;
+      .icon-dog {
+        background: rgba(0, 0, 0, .05)
       }
-
-      .icon-dashboard {
-        background: #36a3f7;
-      }
-
-      .icon-star {
-        background: #f4516c;
-      }
-
-      .icon-shopping {
-        background: rgba(0, 0, 0, .5)
-      }
-    }
-
-    .icon-people {
-      color: #40c9c6;
-    }
-
-    .icon-dashboard {
-      color: #36a3f7;
-    }
-
-    .icon-star {
-      color: #f4516c;
-    }
-
-    .icon-shopping {
-      color: rgba(0, 0, 0, .5)
     }
 
     .card-panel-icon-wrapper {
