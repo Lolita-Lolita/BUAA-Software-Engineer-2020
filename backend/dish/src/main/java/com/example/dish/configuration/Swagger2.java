4f30dbb907ea16092e3d83f379238b55b49103d4
simpleuser <xx@xxxxx.com>
2021-02-06 17:55:10 +0800

    增加加强版的swagger文档

--- backend/dish/src/main/java/com/example/dish/configuration/Swagger2.java
+++ backend/dish/src/main/java/com/example/dish/configuration/Swagger2.java
@@ -12,9 +12,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
-@ConditionalOnProperty(name = "swagger.enable", havingValue = "true")
+//@Configuration
+//@EnableSwagger2
+//@ConditionalOnProperty(name = "swagger.enable", havingValue = "true")
 public class Swagger2 {
 
     @Bean
